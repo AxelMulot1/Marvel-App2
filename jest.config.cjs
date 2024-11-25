27a6f8d06e8d9d1d74fc90b76ca1ecab26f9f0aa
amulot <axel.mulot@etu.univ-poitiers.fr>
2024-11-25 18:16:58 +0100

    clean

---
+++ jest.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  // transform js and jsx files with babel-jest
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  collectCoverageFrom: [
+    // Collect coverage from all js or jsx files in src folder
+    "src/**/*.{js,jsx}", 
+    // Exclude test files from coverage
+    "!src/**/*.test.{js,jsx}", 
+    // Exclude main.jsx from coverage
+    "!src/main.jsx",
+    // Exclude App.jsx from coverage
+    "!src/App.jsx", 
+    // Exclude routes.jsx from coverage
+    "!src/routes.jsx",
+  ],
+  testResultsProcessor: 'jest-sonar-reporter',
+};
